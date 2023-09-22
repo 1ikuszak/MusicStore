fc8f13b14abb234c13c8962a4ed62622ff21434e
1ikuszak <luke.glica@gmail.com>
2023-09-22 17:26:46 +0200

    Data persistence

--- MusicStore/Views/MainWindow.axaml.cs
+++ MusicStore/Views/MainWindow.axaml.cs
@@ -1,11 +1,32 @@
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using MusicStore.ViewModels;
+using ReactiveUI;
 
 namespace MusicStore.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+        
+        // this.WhenActivated is a method provided by ReactiveUI.
+        // It's used to set up event handlers or actions that should occur
+        // when the main window is activated or becomes active.
+        this.WhenActivated(action =>
+            action(ViewModel!.ShowDialog.RegisterHandler(DoShowDialogAsync)));
+    }
+    
+    // Interaction between main window and dialog window
+    private async Task DoShowDialogAsync(InteractionContext<MusicStoreViewModel, 
+        AlbumViewModel?> interaction)
+    {
+        var dialog = new MusicStoreWindow();
+        dialog.DataContext = interaction.Input;
+
+        var result = await dialog.ShowDialog<AlbumViewModel?>(this);
+        interaction.SetOutput(result);
     }
 }
---
+++ MusicStore/Views/MusicStoreWindow.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using System;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using MusicStore.ViewModels;
+using ReactiveUI;
+
+namespace MusicStore.Views;
+
+public partial class MusicStoreWindow : ReactiveWindow<MusicStoreViewModel>
+{
+    public MusicStoreWindow()
+    {
+        InitializeComponent();
+        this.WhenActivated(d => d(ViewModel!.BuyMusicCommand.Subscribe(Close)));
+    }
+}
